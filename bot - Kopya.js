999e0bd038212482d0ca8ed039904fdd976726e5
aznbot <51493909+aznbot@users.noreply.github.com>
2019-06-12 21:47:15 +0300

    Update bot - Kopya.js

--- bot - Kopya.js
+++ bot - Kopya.js
@@ -17,15 +17,15 @@ client.on('message', msg => {
 	  return;
   }
   if (msg.content.toLowerCase() === prefix + 'ping') {
-    msg.reply('Pong! **' + client.ping + '** ms');
+    msg.reply('Ping degeriniz __AZN__ tarafından ölçüldü ve **' + client.ping + '** **ms** olarak belirlendi. ');
   }
-  if (msg.content.toLowerCase() === prefix + 'sigaraic') {
-    msg.send('Sigara İçiyorum!');
+  if (msg.content.toLowerCase() === prefix + 'sigara') {
+    msg.send('Niye sigara içtiriyorsun! Neyse içeyim bi dal..');
     msg.edit(' :smoking: :cloud: :cloud: :cloud: ');
     msg.edit(' :smoking: :cloud: :cloud: ');
     msg.edit(' :smoking: :cloud:  ');
     msg.edit(' :smoking:  ');
-    msg.edit(' Sigaram Bitti! Bot İçsin Ama Siz İçmeyin!');
+    msg.edit(' AZN`den mesaj var. **Bot İçsin Ama Siz İçmeyin!**');
   }
   if (msg.content.toLowerCase() === prefix + 'yazdır') {
     msg.delete();
@@ -33,13 +33,13 @@ client.on('message', msg => {
   }
   if (msg.content.toLowerCase() === prefix + 'temizle') {
     msg.channel.bulkDelete(100);
-    msg.channel.sendMessage("100 adet mesaj silindi!");
+    msg.channel.sendMessage("AZN için **100** adet mesaj silindi!");
   }
   if (msg.content.toLowerCase() === prefix + 'reboot') {
     if (msg.author.id !== ayarlar.sahip) {
-      msg.reply('Benim yapımcım değilsin!');
+      msg.reply('Benim yapımcım değilsin! KISACASI SEN BİR __**AZN**__ DEGİLSİN !');
     } else {
-      msg.channel.sendMessage(`Bot yeniden başlatılıyor...`).then(msg => {
+      msg.channel.sendMessage(`Bot **AZN** tarafindan yeniden başlatiliyor...`).then(msg => {
       console.log(`BOT: Bot yeniden başlatılıyor...`);
       process.exit(0);
     })

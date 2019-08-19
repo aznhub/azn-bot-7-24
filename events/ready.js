dec8ba954aedfec69acee3d87187726700120824
aznhub <46851228+aznhub@users.noreply.github.com>
2019-08-19 10:13:56 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,5 +9,5 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: AZN efendimizin botu aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-client.user.setGame("   ?yardım - |  AZN Impact DEVREDE !  | - __ " + client.guilds.size + " Sunucum ve " + client.users.size + " Kullanıcım var. __ ", "https://twitch.tv/scarew0");
+client.user.setGame("  YAŞAMA ki FAZLA!  'Amaçsız ve aptal çocuklar için sinekler ne ise, biz de tanrılar için oyuz. Eğlenmek için öldürüyorlar.' ", "https://twitch.tv/scarew0");
 }

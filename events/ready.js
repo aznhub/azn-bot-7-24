a9345548ab2fdb5d782837f35dc2f3446636216c
aznhub <46851228+aznhub@users.noreply.github.com>
2019-09-28 10:29:11 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,5 +9,5 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: AZN efendimizin botu aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-client.user.setGame(" YAŞAMA ki FAZLA! 'Aptal çocuklar için sinekler ne ise, tanrılar içinde biz oyuz. Eğlenmeyin, yaratılani öldürerek.' ", "https://twitch.tv/scarew0");
+client.user.setGame(" "Merhaba arkadaş. Merhaba arkadaş. Çok ezikçe. Belki sana bir isim vermeliyim ama bu çok saçma olur. Sen sadece bir düşüncesin, bunu unutmamaliyiz. Siktir, bu gerçekten oldu. Hayali bir insanla konuşuyorum. Size anlatacağım şey çok gizli, hepimizi aşan bir komplo. Dışarıda dünyayı yöneten güçlü insanlardan oluşan gizli bir grup var. Size şimdi, görünmez ve hakkında hiç bir şey bilinmeyen insanlardan bahsedeceğim. Yüzde birin üstünde ki yüzde bire sahip, izne gerek duymadan tanrıyı oynayan insanlardan. Ve şimdi de sanırım beni takip ediyorlar."", "https://twitch.tv/scarew0");
 }

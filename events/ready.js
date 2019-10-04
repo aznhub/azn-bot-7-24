5019e0065791350187ab0d0b1fa5b612b4c47b3f
aznhub <46851228+aznhub@users.noreply.github.com>
2019-10-04 10:25:12 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,5 +9,5 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: AZN efendimizin botu aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-client.user.setGame(" YAŞAMA ki FAZLA! 'Aptal çocuklar için sinekler ne ise, tanrılar içinde biz oyuz. Eğlenmeyin, yaratılani öldürerek.' ", "https://twitch.tv/scarew0");
+client.user.setGame(" Yssssl çocuklar için sinekler ne ise, tanrılar içinde biz oyuz. Eğlenmeyin, yaratılani öldürerek.' ", "https://twitch.tv/scarew0");
 }
